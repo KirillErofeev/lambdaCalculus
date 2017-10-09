3d8d2b357b0732994545c2b8f6c5b4ba87786413
Mansur Ziiatdinov <gltronred@gmail.com>
2017-10-09 22:56:02 +0300

    S1 stub

---
+++ sem1/src/Main.hs
@@ -0,0 +1,79 @@
+module Main where
+
+newtype Symbol = Symbol { unSymbol :: String } deriving (Eq,Show,Read)
+
+-- (1)
+data TermS = SymS Symbol        -- x
+           | LamS Symbol TermS  -- \x -> t
+           | AppS TermS TermS   -- t1 t2
+           deriving (Eq,Show,Read)
+
+-- (1)
+-- переименовать все переменные так, чтобы все они были разными.
+alpha :: TermS -> TermS
+alpha = error "Implement me!"
+
+-- (1)
+-- один шаг редукции, если это возможно. Стратегия вычислений - полная, т.е. редуцируются все возможные редексы.
+beta :: TermS -> Maybe TermS
+beta = error "Implement me!"
+
+-- (2)
+data TermI = SymI Int
+           | LamI TermI
+           | AppI TermI TermI
+           deriving (Eq,Show,Read)
+
+-- (2)
+-- перевод выражения в TermI
+toTermI :: TermS -> TermI
+toTermI = error "Implement me!"
+
+-- (2)
+-- шаг редукции
+betaI :: TermI -> Maybe TermI
+betaI = error "Implement me!"
+
+data TermP = TermP TermS
+           -- (3)
+           | Boolean Bool
+           | Iff Bool TermP TermP
+           | Not TermP
+           | And TermP TermP
+           | Or TermP TermP
+           -- (4)
+           | Natural Int
+           | Plus TermP TermP
+           | Mult TermP TermP
+           -- (4*) +10%
+           | Minus TermP TermP
+           | Divide TermP TermP
+           -- (5*) +50%
+           | Y TermP
+           -- (5**) +50%
+           -- mutually recursive
+           -- (6)
+           | Pair TermP TermP
+           | Fst TermP
+           | Snd TermP
+           -- (7)
+           | Cons TermP TermP
+           | IsNil TermP
+           | Head TermP
+           | Tail TermP
+           deriving (Eq,Show,Read)
+
+toTermS :: TermP -> TermS
+toTermS = error "Implement me!"
+
+solve :: TermP -> Either (Maybe TermI) (Maybe TermS)
+solve = error "Choose your variant"
+-- (1)
+-- solve = Right . beta . alpha . toTermS
+-- (2)
+-- solve = Left . betaI . toTermI . toTermS
+
+main :: IO ()
+main = do
+  s <- readLn
+  print $ solve s
