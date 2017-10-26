71299ca26f9755b1e4be405ee21f19f4d72d9aec
Mansur Ziiatdinov <gltronred@gmail.com>
2017-10-26 19:17:00 +0300

    Remove wrong code

--- sem1/src/lang101/Lang.hs
+++ sem1/src/lang101/Lang.hs
@@ -13,14 +13,10 @@ data Simple = Const Name
 
 data PrologTerm = Sim Simple
                 | Relation Name [Simple]
-                | Function Name [Name] [PrologTerm]
+                | Implies Name [Name] [PrologTerm]
                 deriving (Eq,Show,Read)
 
 type Prolog = [PrologTerm]
-
-data Ans = BoolAns Bool
-         | VarAns [(Name, Simple)]
-         deriving (Eq,Show,Read)
 
 data Fact = Ident Simple
           | Rel Name [Simple]
@@ -31,48 +27,12 @@ toIdent :: Fact -> Maybe Simple
 toIdent (Ident s) = Just s
 toIdent _ = Nothing
 
-constructSubst :: [Name] -> [Simple] -> [[(Name,Simple)]]
-constructSubst [] _ = []
-constructSubst [x] facts = map (pure . (x,)) facts
-constructSubst (arg : args) facts = let
-  firstSubst = map (arg,) facts
-  restSubst = constructSubst args facts
-  in [first : rest | first <- firstSubst, rest <- restSubst]
-
-substitute :: [(Name, Simple)] -> Simple -> Simple
-substitute subst (Var name) = case lookup name subst of
-  Nothing -> Var name
-  Just s -> s
-substitute _ s = s
-
-applySubst :: Name -> [Simple] -> [(Name,Simple)] -> Fact
-applySubst relName relArgs subst =
-  Rel relName $ map (substitute subst) relArgs
-
-apply :: Name -> [Name] -> Knowledge -> PrologTerm -> Knowledge
-apply n args knowledge (Sim s) = [Ident s]
-apply n args knowledge (Relation m ts) = let
-  allConsts = mapMaybe toIdent knowledge
-  allArgs = constructSubst args allConsts
-  in map (applySubst n ts) allArgs
-
-updateKnowledgeBase :: Knowledge
-                    -> PrologTerm
-                    -> Knowledge
-updateKnowledgeBase ks (Sim s) = Ident s : ks
-updateKnowledgeBase ks (Relation n ss) = Rel n ss : ks
-updateKnowledgeBase ks (Function n args terms) =
-  concatMap (apply n args ks) terms ++ ks
-
-eval :: Prolog -> (Name, [Simple]) -> Ans
-eval terms (name, args) = error "Implement me!"
-
 
 test101 :: Prolog
 test101 =
   [ Relation "human" [Const "sokrat"]
   , Relation "human" [Const "platon"]
-  , Function "mortal" ["Someone"]
+  , Implies "mortal" ["Someone"]
     [ Relation "human" [Var "Someone"]
     ]
   ]
